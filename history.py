e26553379f6e918c4632f68de3311b93ee71503a
dwpaley <dwpaley@gmail.com>
2018-08-19 10:51:26 -0400

    Added a few comments and made the get_history functions private
    (...to simplify the help text)

--- history.py
+++ history.py
@@ -6,7 +6,7 @@ def _hprint(hlist):
     for i, line in hlist:
         print('{}\t{}'.format(i, line))
 
-def get_history_full():
+def _get_history_full():
     '''returns an enumerated list of history (numbered from the absolute
     beginning)
     '''
@@ -15,17 +15,17 @@ def get_history_full():
     for i in range(hlen): h.append(readline.get_history_item(i+1))
     return list(enumerate(h))
 
-def broken_get_history_full():
+def _broken_get_history_full():
     '''this seems to only grab every other line?? no idea why it's not 
-    equivalent to the explicit loop in get_history_full.
+    equivalent to the explicit loop in _get_history_full.
     '''
     hlen = readline.get_current_history_length()
     history = [readline.get_history_item(i+i) for i in range(hlen)]
     return history
 
-def get_history_current():
+def _get_history_current():
     h = []
-    hfull = get_history_full()
+    hfull = _get_history_full()
     for i, line in hfull:
         if (line.lower().startswith('quit()') or 
                 line.lower().startswith('exit()')):
@@ -35,27 +35,32 @@ def get_history_current():
     return h
 
 def history():
-    _hprint(get_history_current())
+    '''Print all commands entered in current session
+    '''
+    _hprint(_get_history_current())
 
 def history_full():
-    _hprint(get_history_full())
+    '''Print all commands entered ever
+    '''
+    _hprint(_get_history_full())
 
 def recall(n):
     '''execute command n from the full history
     '''
-    exec(get_history_full()[n][1])
+    exec(_get_history_full()[n][1])
 
 def recall_range(n1, n2):
     '''Execute commands between n1 and n2 (python slice style)
+    This currently stops executing if it hits a command that throws an error
     '''
-    exec('\n'.join(item[1] for item in get_history_full()[n1:n2]))
+    exec('\n'.join(item[1] for item in _get_history_full()[n1:n2]))
 
 def find(term):
     '''print lines matching term. For now it's a simple string find, case-
     insensitive by default.
     '''
     _hprint([
-        (i, line) for i, line in get_history_current()
+        (i, line) for i, line in _get_history_current()
         if term.lower() in line.lower()
         ])
 
