701f631ffcb581d54f8b02ead7c1496288703873
dwpaley <dwpaley@gmail.com>
2018-08-19 11:05:04 -0400

    made docstring more helpful

--- history.py
+++ history.py
@@ -1,5 +1,9 @@
 '''
 BASH-esque history for the Python3 interactive interpreter.
+
+Quickstart: 
+    -Try history.history(), equivalent to bash $ history
+    -Try history.recall(n), equivalent to bash $ !n
 '''
 
 import readline
